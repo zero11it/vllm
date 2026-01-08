f5cc6169a5242e09d5ac0bb359c68d7c70e3157b
Davide Ferri <d.ferri@zero11.it>
2026-01-08 17:25:40 +0100

    Release 0.13.0.1 - Patch mistral validator + remove ghcr.io

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM vllm/vllm-openai:v0.13.0
 
 COPY start.sh /app/
 
+RUN sed -i 's/{Roles.assistant, Roles.tool}/{Roles.assistant, Roles.user, Roles.tool}/g' /usr/local/lib/python3.12/dist-packages/mistral_common/protocol/instruct/validator.py
+
 ENTRYPOINT ["/app/start.sh"]
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-version=$(cat Dockerfile|head -1|cut -d ":" -f 2)
-docker buildx build --provenance=false --sbom=false --platform linux/amd64 -f Dockerfile --push --tag ghcr.io/zero11it/vllm:$version .
+version=$(cat Dockerfile|head -1|cut -d ":" -f 2).1
 docker buildx build --platform linux/amd64 -f Dockerfile --push --tag public.registry.zero11.org/zero11/vllm:$version .
